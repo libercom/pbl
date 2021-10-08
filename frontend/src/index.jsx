502dd2939e6cae86da9eea18872d7faf93f9c43f
Vasile <ignatvasea@gmail.com>
2021-10-08 17:40:13 +0300

    Initialized project

---
+++ frontend/src/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+    return (
+        <div>
+            <h1>Fuck you</h1>
+            <p>george</p>
+        </div>
+    )
+}
+
+const root = document.getElementById('root')
+ReactDOM.render(<App />, root)
---
+++ frontend/webpack.config.js
@@ -0,0 +1,26 @@
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin')
+
+module.exports = {
+    module: {
+        rules: [
+            {
+                test: /\.jsx$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            }
+        ],
+    },
+    plugins: [
+        new BrowserSyncPlugin({
+            notify: false,
+            files: [
+                './templates/frontend/*.html',
+                './src/*.jsx'
+            ],
+            // server: { baseDir: ['src'] },
+            proxy: '127.0.0.1:8000'
+        })
+    ]
+};
