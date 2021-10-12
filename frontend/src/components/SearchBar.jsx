a8156427fca1e017ecf07074bf2f30deb8cf457d
Vasile <ignatvasea@gmail.com>
2021-10-12 12:38:59 +0300

    Changed webpack config

--- frontend/src/components/SearchBar.jsx
+++ frontend/src/components/SearchBar.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const SearchBar = () => {
     return (
         <nav className="navbar navbar-light justify-content-center">
-            <Link to="/" class="navbar-brand">Ecatalog</Link>
+            <Link to="/" className="navbar-brand">Ecatalog</Link>
             <form className="form-inline d-flex" style={{ width: '70%', marginLeft: '70px' }} onSubmit={event => event.preventDefault()}>
                 <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" autoComplete="false" />
                 <button className="btn btn-outline-success my-2 my-sm-0" style={{ marginLeft: '20px' }} type="submit">Search</button>
--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,25 +1,45 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const BrowserSyncPlugin = require('browser-sync-webpack-plugin')
 
 module.exports = {
+    entry: path.join(__dirname, "src", "index.jsx"),
     module: {
         rules: [
             {
-                test: /\.jsx$/,
+                test: /\.(jsx|js)$/,
                 exclude: /node_modules/,
                 use: {
-                    loader: "babel-loader"
+                    loader: "babel-loader",
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/preset-react']
+                    }
                 }
-            }
+            },
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
         ],
     },
-    // plugins: [
-    //     new BrowserSyncPlugin({
-    //         notify: false,
-    //         files: [
-    //             './templates/frontend/*.html',
-    //             './src/*.jsx'
-    //         ],
-    //         proxy: '127.0.0.1:8000'
-    //     })
-    // ]
-};
+    output: {
+        path: path.resolve(__dirname, "./static"),
+        filename: 'main.js'
+    },
+    resolve: {
+        extensions: ['*', '.js', '.jsx'],
+    },
+    plugins: [
+        // new BrowserSyncPlugin({
+        //     notify: false,
+        //     files: [
+        //         './templates/frontend/*.html',
+        //         './src/*.jsx'
+        //     ],
+        //     proxy: '127.0.0.1:8000'
+        // }),
+        // new HtmlWebpackPlugin({
+        //     template: path.join(__dirname, "src", "index.html"),
+        // }),
+    ]
+}
